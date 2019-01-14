b360b734dd7f687425771fd7b30f6310a87f2f98
Alex Meyer <alexwreyem@gmail.com>
2019-01-14 09:00:33 -0500

    Updated code with custom delay function, plus blackouts between columns.

--- ImageToLED/ImageToLED.ino
+++ ImageToLED/ImageToLED.ino
@@ -7,19 +7,19 @@
 #define LEFT_PIN 11
 #define RIGHT_PIN 12
 // #define RIGHT_PIN 6
-#define DELAY 50
+#define ON_TIME 10
+#define OFF_TIME 15
 
 #define TMP_BRIGHTNESS 32
+
+unsigned long prevMillis = 0;
 
 CRGB leds[ROWS];
 
 
-void setup() {
-  FastLED.addLeds<NEOPIXEL, LEFT_PIN>(leds, 0, NUM_LEDS_PER_WING);
-  FastLED.addLeds<NEOPIXEL, RIGHT_PIN>(leds, NUM_LEDS_PER_WING, NUM_LEDS_PER_WING);
-}
-
 void setLED(int led, CRGB color) {
+  // This part reverses the order of LEDs for one wing,
+  // as they normally start at 0 in the inside, and I'm addressing both together
   if (led < NUM_LEDS_PER_WING) {
     led = NUM_LEDS_PER_WING - 1 - led;
   }
@@ -33,25 +33,45 @@ void showStrip () {
   FastLED.show();
 }
 
+void myDelay(int delay) {
+  // This just waits the "remainder" of the time until the next interval.
+  // This is better than delay(), as the actual timing between tasks will be correct
+  while (millis() - prevMillis < delay); // Just loop until it's time
+  prevMillis = millis();
+}
+
+void blackout() {
+  for (int i = 0; i < ROWS; i++) {
+    leds[i] = CRGB::Black;
+  }
+}
+
+void setup() {
+  // Adding both outputs to the same leds[] array
+  FastLED.addLeds<NEOPIXEL, LEFT_PIN>(leds, 0, NUM_LEDS_PER_WING); // From 0-(one wing)
+  FastLED.addLeds<NEOPIXEL, RIGHT_PIN>(leds, NUM_LEDS_PER_WING, NUM_LEDS_PER_WING); // from (one wing)-end
+}
+
 void loop() {
   int count = 0;
   for (int x = 0; x < COLS; x++) {
     for (int y = 0; y < ROWS; y++) {
-        byte currentByte = pgm_read_byte(&image[count]);
+        byte currentByte = pgm_read_byte(&image[count]); // count is only updated every other time, because we use one byte for two pixels
         if (y % 2 == 0) {
-          setLED(y, colors[currentByte >> 4]);
+          setLED(y, colors[currentByte >> 4]); // first led gets the first half of the byte ( 0x(F)F ), shifted over
         } else {
-          setLED(y, colors[currentByte & 0x0F]);
+          setLED(y, colors[currentByte & 0x0F]); // second led gets the second half of the byte ( 0xF(F) ) with the first half removed
           count++;
         }
     }
-    showStrip();
-    delay(DELAY);
+    myDelay(OFF_TIME);  // Delay the remainder of the "off time" between columns
+    showStrip();        // Show the new "on" values that were queued
+    blackout();         // Prepare the blackout for between columns
+    myDelay(ON_TIME);   // Delay the remainder of the "on time" for the columns
+    showStrip();        // Show the new "blacked out" values that were queued
   }
-  for (int i = 0; i < ROWS; i++) {
-    leds[i] = CRGB::Black;
-  }
+  // Between images, give some space
+  blackout();
   showStrip();
-  // Serial.println();
   delay(3000);
 }
